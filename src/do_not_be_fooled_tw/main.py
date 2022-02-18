2e3d48f9069b4e73309a76632678cd94a21a84aa
Wei-Fan Chiang <weifan.wf@gmail.com>
2022-02-18 20:43:50 +0000

    add an option to turn off the debug logging

--- src/do_not_be_fooled_tw/main.py
+++ src/do_not_be_fooled_tw/main.py
@@ -1,5 +1,7 @@
 import os 
 import click 
+import logging 
+import warnings
 from . import web_crawler 
 
 # ====
@@ -9,15 +11,29 @@ from . import web_crawler
 @click.option('--start-at', type=str, required=True)
 @click.option('--life-in-sec', type=int, required=True)
 @click.option('--output', type=str, required=True)
+@click.option('--debug', type=bool, default=False)
 def main(
     start_at :str, 
     life_in_sec :int, 
-    output :str
-): 
+    output :str,
+    debug :bool
+):     
+    # Turn off warning if not in the debug mode 
+    if (not debug): 
+        warnings.filterwarnings('ignore')
+
+    # Find out where this web_crawler.py is 
     web_crawler_path = str(web_crawler.__file__)
-    os.system(f'scrapy runspider {web_crawler_path} -a start_at={start_at} -a life_in_sec={life_in_sec} -a output={output}')
+    print('Web Crawler Path: {}'.format(web_crawler_path))
+
+    # Compose the scrapy command 
+    scrapy_command = f'scrapy runspider {web_crawler_path} -a start_at={start_at} -a life_in_sec={life_in_sec} -a output={output}'
+    if (not debug): 
+        scrapy_command = scrapy_command + ' --nolog' 
+
+    # Run the scrapy command 
+    os.system(scrapy_command)
     
-    print(web_crawler_path)
 
 if __name__ == '__main__': 
     main() 
